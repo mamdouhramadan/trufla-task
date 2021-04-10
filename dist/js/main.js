bfa3ae6cfbad3eb67514b467871207f6e6de669d
mamdouhramadan <mamdouhalamassi@gmail.com>
2021-04-10 08:14:24 +0200

    init Service Worker Script

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,3 +23,24 @@ $(document).ready(function () {
   }
 
 });
+
+$(window).on('load', function () {
+
+  registerSW()
+
+})
+
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js', {
+      scope: '.' // <--- THIS BIT IS REQUIRED
+    }).then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  }
+
+}
